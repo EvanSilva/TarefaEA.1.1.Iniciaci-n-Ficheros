5588441d5a8846bcb75a98fffaa197badabf5c42
Evan Silva <evanchapela@gmail.com>
2024-09-19 11:07:40 +0200

    build(Ordenaciones): finish method

--- src/Ordenaciones.java
+++ src/Ordenaciones.java
@@ -1,7 +1,10 @@
-import java.lang.reflect.Array;
-import java.nio.file.Path;
-import java.util.Arrays;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 /**
  * 3. Ordenaciones
@@ -17,8 +20,72 @@ public class Ordenaciones {
         Scanner sc = new Scanner(System.in);
         System.out.println("Provee la ruta al archivo para ordenar");
         String ruta = sc.nextLine();
-        System.out.println("Selecciona el tipo de ordenacion: \nA: ascendente case-sensitive: \nB: ascendente case-insensitive \nC: descendente case-sensitive \nD: descendente case-insensitive ");
+        System.out.println("Selecciona el tipo de ordenacion: \na: ascendente case-sensitive: \nb: ascendente case-insensitive \nc: descendente case-sensitive \nd: descendente case-insensitive ");
         String tipoOrdenacion = sc.nextLine();
+
+        List<String> lineas = new ArrayList<>();
+
+        List<String> lineasOrdenadas = new ArrayList<>();
+        String apendiceFinal = "";
+
+        String nuevaRuta = ruta.substring(0, ruta.length() - 4) ;
+
+        try (BufferedReader lector = new BufferedReader(new
+
+                FileReader(ruta));) {
+
+            String linea = "";
+
+            while ((linea = lector.readLine()) != null)
+
+                lineas.add(linea);
+
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+
+        if (tipoOrdenacion.equals("a")) {
+            lineasOrdenadas = lineas.stream()
+                    .sorted()
+                    .collect(Collectors.toList());
+
+            nuevaRuta += "_asc_case.txt";
+        }
+
+        if (tipoOrdenacion.equals("b")) {
+            lineasOrdenadas = lineas.stream()
+                    .sorted(String.CASE_INSENSITIVE_ORDER)
+                    .collect(Collectors.toList());
+
+            nuevaRuta += "_asc_non_case.txt";
+        }
+
+        if (tipoOrdenacion.equals("c")) {
+            lineasOrdenadas = lineas.stream()
+                    .sorted(Comparator.reverseOrder())
+                    .collect(Collectors.toList());
+            nuevaRuta += "_desc_case.txt";
+        }
+
+        if (tipoOrdenacion.equals("d")) {
+            lineasOrdenadas = lineas.stream()
+                    .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+                    .collect(Collectors.toList());
+            nuevaRuta += "_desc_non_case.txt";
+        }
+
+        try (BufferedWriter escritor = new BufferedWriter(new FileWriter(nuevaRuta,false));) {
+
+            for(String linea : lineasOrdenadas) {
+                escritor.write(linea);
+                escritor.newLine();
+            }
+
+            System.out.println("Escritura realizada.");
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
 
     }
 }
